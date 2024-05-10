f6435c59c558b648e55c199e88647fbe332fdf87
jawher.bouhouch <jawher.bouhouch@maloon.de>
2024-05-10 16:00:09 +0200

    add basic middlewares

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/jawherbou/url_analyzer/server/middlewares"
 	"github.com/jawherbou/url_analyzer/server/routes"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,6 +11,7 @@ func main() {
 	app := fiber.New()
 
 	// Middlewares
+	middlewares.FiberMiddleware(app) // Register Fiber's middleware for app.
 
 	// Routes
 	routes.PublicRoutes(app)  // Register a public routes for app.
