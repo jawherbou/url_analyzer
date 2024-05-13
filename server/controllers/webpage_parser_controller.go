0debfdb9f0cd76dff03d9288ed46c447654e3c4a
jawher.bouhouch <jawher.bouhouch@maloon.de>
2024-05-13 20:19:53 +0200

    apply concurrency improvement

--- server/controllers/webpage_parser_controller.go
+++ server/controllers/webpage_parser_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"strings"
+	"sync"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jawherbou/url_analyzer/server/services/parsers"
@@ -34,20 +35,38 @@ func GetAnalysis(c *fiber.Ctx) error {
 	headingsParser := parsers.NewHeadingsParser()
 	linksParser := parsers.NewLinksParser()
 
-	// get text
-	titleParser.Parse(string(body), analysis)
+	// Create a wait group to wait for all goroutines to finish
+	var wg sync.WaitGroup
+	wg.Add(5) // Number of goroutines
 
-	// get html version
-	htmlVersionParser.Parse(string(body), analysis)
+	// Run the function calls concurrently using goroutines
+	go func() {
+		defer wg.Done()
+		titleParser.Parse(string(body), analysis)
+	}()
 
-	// set login form status
-	loginFormParser.Parse(string(body), analysis)
+	go func() {
+		defer wg.Done()
+		htmlVersionParser.Parse(string(body), analysis)
+	}()
 
-	// add headings
-	headingsParser.Parse(string(body), analysis)
+	go func() {
+		defer wg.Done()
+		loginFormParser.Parse(string(body), analysis)
+	}()
 
-	// add links
-	linksParser.Parse(string(body), host, analysis)
+	go func() {
+		defer wg.Done()
+		headingsParser.Parse(string(body), analysis)
+	}()
+
+	go func() {
+		defer wg.Done()
+		linksParser.Parse(string(body), host, analysis)
+	}()
+
+	// Wait for all goroutines to finish
+	wg.Wait()
 
 	// Return status 200 OK.
 	return c.JSON(analysis)
